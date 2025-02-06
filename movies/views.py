9e4ff40368076a32e26ee1a17aae97880c6a75da
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2025-02-06 16:31:41 -0500

    fix: reviews system updated

--- movies/views.py
+++ movies/views.py
@@ -6,62 +6,66 @@ from .models import Movie
 
 # Create your views here.
 
+
 def index(request):
-    search_term = request.GET.get('search')
+    search_term = request.GET.get("search")
     if search_term:
         movies = Movie.objects.filter(name__icontains=search_term)
     else:
         movies = Movie.objects.all()
     template_data = {}
-    template_data['title'] = 'Movies'
-    template_data['movies'] = movies
-    return render(request, 'movies/index.html',
-                  {'template_data': template_data})
+    template_data["title"] = "Movies"
+    template_data["movies"] = movies
+    return render(request, "movies/index.html", {"template_data": template_data})
+
+
 def show(request, id):
     movie = Movie.objects.get(id=id)
     reviews = Review.objects.filter(movie=movie)
     template_data = {}
-    template_data['title'] = movie.name
-    template_data['movie'] = movie
-    template_data['reviews'] = reviews
-    return render(request, 'movies/show.html',
-                  {'template_data': template_data})
+    template_data["title"] = movie.name
+    template_data["movie"] = movie
+    template_data["reviews"] = reviews
+    return render(request, "movies/show.html", {"template_data": template_data})
+
 
 @login_required
 def create_review(request, id):
-    if request.method == 'POST' and request.POST['comment'] != '':
+    if request.method == "POST" and request.POST["comment"] != "":
         movie = Movie.objects.get(id=id)
         review = Review()
-        review.comment = request.POST['comment']
+        review.comment = request.POST["comment"]
         review.movie = movie
         review.user = request.user
         review.save()
-        return redirect('movies.show', id=id)
+        return redirect("movies.show", id=id)
     else:
-        return redirect('movies.show', id=id)
+        return redirect("movies.show", id=id)
+
 
 @login_required
 def edit_review(request, id, review_id):
+    movie = get_object_or_404(Movie, id=id)
     review = get_object_or_404(Review, id=review_id)
+
+    # Check if the user owns this review
     if request.user != review.user:
-        return redirect('movies.show', id=id)
-    if request.method == 'GET':
-        template_data = {}
-        template_data['title'] = 'Edit Review'
-        template_data['review'] = review
-        return render(request, 'movies/edit_review.html',
-            {'template_data': template_data})
-    elif request.method == 'POST' and request.POST['comment'] != '':
-        review = Review.objects.get(id=review_id)
-        review.comment = request.POST['comment']
-        review.save()
-        return redirect('movies.show', id=id)
-    else:
-        return redirect('movies.show', id=id)
+        return redirect("movies.show", id=id)
+
+    if request.method == "POST":
+        comment = request.POST.get("comment")
+        if comment:
+            review.comment = comment
+            review.save()
+            return redirect("movies.show", id=movie.id)
+
+    # For GET request, show the edit form
+    template_data = {"title": "Edit Review", "movie": movie, "review": review}
+    return render(request, "movies/edit_review.html", {"template_data": template_data})
+
 
 @login_required
 def delete_review(request, id, review_id):
-    review = get_object_or_404(Review, id=review_id,
-        user=request.user)
+    review = get_object_or_404(Review, id=review_id, user=request.user)
     review.delete()
-    return redirect('movies.show', id=id)
+    return redirect("movies.show", id=id)
