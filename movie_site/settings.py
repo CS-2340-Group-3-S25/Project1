1113c1b0273e5248a2e1eb327a1e7f6d5f335bdc
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2025-02-11 17:36:27 -0500

    fix: switch to mysql

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "movie_site.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "movie_site_db",
+        "USER": "superuser",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
