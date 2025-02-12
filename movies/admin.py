3765751e3d5871e289e0b992e816c2b31f385e66
31465 <aamir31465@gmail.com>
2025-02-12 18:28:41 -0500

    Cart Test

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+
 from .models import Movie, Review
+
+from .models import Movie
+from .models import Order
+
 
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
@@ -8,4 +13,7 @@ class MovieAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Movie, MovieAdmin)
+
 admin.site.register(Review)
+
+admin.site.register(Order)
