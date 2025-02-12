c800b434a89b904d4ba8f920fc5ec0dca16f2805
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2025-02-12 18:56:11 -0500

    cleanup: add reviews dump

--- movies/models.py
+++ movies/models.py
@@ -29,3 +29,8 @@ class Review(models.Model):
 
     def __str__(self):
         return str(self.id) + " - " + self.movie.name
+
+    @classmethod
+    def load_initial_data(cls):
+        if not cls.objects.exists():
+            call_command("loaddata", "reviews.json", app_label="movies")
