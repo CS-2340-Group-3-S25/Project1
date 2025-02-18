d649c592825614df75f186677c16a8d72fb95a2f
Arnav Jain <arnavjain2808@gmail.com>
2025-02-18 16:34:16 -0500

    Edited UI components, changed button colors, fixed styling and layout

--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from movies.models import Movie  # Import the Movie model
 
 # Create your views here.
 
 def index(request):
     template_data = {}
     template_data['title'] = 'Movies Store'
+    template_data['movies'] = Movie.objects.all()[:4]  # Get the first 4 movies
     return render(request, 'home/index.html', {'template_data': template_data})
 def about(request):
     template_data = {}
